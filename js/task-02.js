9a79ecb797b84d6f0ed7cf89e66dd0fb7be39f1a
Rudenko0339 <rudenko0339@gmail.com>
2022-11-05 11:47:57 +0200

    end of 6HW

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,20 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const liItems = document.querySelector("#ingredients");
+const liArray = [];
+
+ingredients.forEach((ingredient) => {
+  const item = document.createElement("li");
+  item.className = "item";
+  item.textContent = ingredient;
+  liArray.push(item);
+});
+liItems.append(...liArray);
+console.log(liItems);
--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,13 @@
+const formElem = document.querySelector(".login-form");
 
+function onFormLoginSubmit(e) {
+  e.preventDefault();
+  const { email, password } = e.currentTarget;
+  if (email.value === "" || password.value === "") {
+    return alert("Всі поля повинні бути заповнені !");
+  }
+  const formData = { email: email.value, password: password.value };
+  console.log(formData);
+  e.currentTarget.reset();
+}
+formElem.addEventListener("submit", onFormLoginSubmit);
